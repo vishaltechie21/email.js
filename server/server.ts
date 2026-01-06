abc226e860e9bf53b49cb01eeef8bf7e1256338b
vishaltechie21 <developervishal8@gmail.com>
2026-01-06 12:53:00 +0530

    asdf

--- server/server.ts
+++ server/server.ts
@@ -4,9 +4,9 @@ import path from 'path';
 import { sendEmail, IntegrationEmailConfig } from './utils/email';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors()); // Allow all origins explicitly for this setup
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
 
